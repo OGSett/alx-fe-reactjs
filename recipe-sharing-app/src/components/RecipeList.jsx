81e9d0eebf074185b9beeaf6ca538af03bec63b9
ogsett <ecomseth@gmail.com>
2025-07-27 14:53:39 +0100

    addeding task 1 for State management in React

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,18 @@
+import { useRecipeStore } from '../store/recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} style={{ marginBottom: '1rem' }}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
