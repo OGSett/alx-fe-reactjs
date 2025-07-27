bbe13db0df822163ab626e36d801edabef1879d3
ogsett <ecomseth@gmail.com>
2025-07-27 15:21:23 +0100

    pushing task 3 for State management in React

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,11 +2,11 @@ import { useRecipeStore } from './recipeStore';
 import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
-      {recipes.map((recipe) => (
+      {filteredRecipes.map((recipe) => (
         <div key={recipe.id} style={{ marginBottom: '1rem' }}>
           <Link to={`/recipe/${recipe.id}`}>
             <h3>{recipe.title}</h3>
---
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import { useRecipeStore } from './recipeStore';
+
+const SearchBar = () => {
+  const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+      style={{ marginBottom: '1rem', padding: '8px', width: '100%' }}
+    />
+  );
+};
+
+export default SearchBar;
