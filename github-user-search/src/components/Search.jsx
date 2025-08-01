9fab64696222ce6ad5c48ac1a2720dee2132e2b9
ogsett <ecomseth@gmail.com>
2025-08-01 19:12:25 +0100

    modifying Search.jsx and githubServices.js to handle variant options for search logic aand displaying more than one user

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -5,16 +5,19 @@ const Search = () => {
     const [inputValue, setInputValue] = useState('')
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
-    const [userData, setUserData] = useState(null)
+    const [userData, setUserData] = useState([])
+    const [location, setLocation] = useState('')
+    const [minRepos, setMinRepos] = useState('')
+    const [page, setPage] = useState(1)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
         setLoading(true)
         setError(false)
-        setUserData(null)
+        setUserData([])
 
         try {
-            const data = await fetchUserData(inputValue)
+            const data = await fetchUserData(inputValue, location, minRepos,page)
             setUserData(data)
         } catch (e) {
             setError(true)
@@ -23,42 +26,56 @@ const Search = () => {
         }
     }
 
+    const handleLoadMore = async () => {
+        const nextPage = page + 1;
+        const moreUsers = await fetchUserData(inputValue, location, minRepos, nextPage);
+        setUserData(prev => [...prev, ...moreUsers]);
+        setPage(nextPage);
+    };
     
 
     return ( 
-        <div className="bg-blue-200 w-full h-svh flex justify-center items-center flex-col">
-        <div>
-            <h1  className="text-3xl font-bold text-gray-700 py-3">GitHub search engine</h1>
-            <form onSubmit={handleSubmit}>
-                <input className="px-4 py-2 bg-white  rounded-md hover:bg-gray-300 transition duration-200 outline-none" placeholder="Enter a unsername" type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-                <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200" type="submit">Search</button>
-            </form>
-        </div>
-        {loading && <p className="my-3 text-blue-600">Loading...</p> }
-        {error && <p className="my-3 text-red-600">Looks like we cant find the user</p> }
-        {userData && (
-           <div className="my-3 max-w-sm w-full bg-white shadow-md rounded-lg p-4 flex flex-col items-center text-center space-y-3">
-                <img
-                    src={userData.avatar_url}
-                    alt="User avatar"
-                    className="w-24 h-24 rounded-full border-2 border-gray-300"
-                />
-                <h3 className="text-xl font-semibold text-gray-800">
-                    {userData.name || "No name provided"}
-                </h3>
-                <p className="text-sm text-gray-600">Username: {userData.login}</p>
-                <a
-                    href={userData.html_url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="mt-2 text-blue-600 hover:text-blue-800 font-medium"
-                >
-                    View Profile
-                </a>
+        <div className="bg-blue-200 w-full h-svh flex items-center flex-col">
+            <div className="min-w-[700px]">
+                <h1  className="text-3xl font-bold text-gray-700 py-3">GitHub search engine</h1>
+                <form onSubmit={handleSubmit} className="flex flex-row gap-2">
+                    <input className="px-4 py-2 bg-white  rounded-md hover:bg-gray-300 transition duration-200 outline-none" placeholder="Enter a unsername" type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+                    <input className="px-4 py-2 bg-white  rounded-md hover:bg-gray-300 transition duration-200 outline-none" placeholder="Enter a location" type="text" value={location} onChange={(e) => setLocation(e.target.value)}/>
+                    <input className="px-4 py-2 bg-white  rounded-md hover:bg-gray-300 transition duration-200 outline-none" placeholder="Enter a number of repos" type="number" value={minRepos} onChange={(e) => setMinRepos(e.target.value)}/>
+                    <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200" type="submit">Search</button>
+                </form>
             </div>
-        )}
+            {loading && <p className="my-3 text-blue-600">Loading...</p> }
+            {error && <p className="my-3 text-red-600">Looks like we cant find the user</p> }
+            {userData.length > 0 && (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4 max-h-[85vh] scroll-hidden overflow-scroll">
+                    {userData.map((user) => (
+                    <div key={user.id} className="bg-white shadow-md rounded-lg p-4 text-center">
+                        <img
+                        src={user.avatar_url}
+                        alt={`${user.login}'s avatar`}
+                        className="w-20 h-20 rounded-full mx-auto"
+                        />
+                        <h3 className="text-lg font-semibold mt-2">{user.login}</h3>
+                        <a
+                        href={user.html_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-600 hover:underline"
+                        >
+                        View Profile
+                        </a>
+
+                    </div>
+                    
+                ))}
+                </div>
+            )}
+            {userData.length > 20 && (
+                <button onClick={handleLoadMore} className="mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"> Load More </button>
+            )}
         </div>
-     );
+    );
 }
  
 export default Search;
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,15 +2,22 @@ import axios from "axios"
 
 const token = import.meta.env.VITE_APP_GITHUB_API_KEY
 
-const fetchUserData = async (username) => {
+const fetchUserData = async (username, location, minRepos, page = 1, perPage = 30) => {
+    const queryPart = []
     const config = {
         headers: {
             Authorization: `Bearer ${token}`
         }
     }
 
-    const res = await axios.get(`https://api.github.com/users/${username}`, config)
-    return res.data
+    if (username) queryPart.push(`${username} in:login`)
+    if (location) queryPart.push(`location:${location}`)
+    if (minRepos) queryPart.push(`repos:>${minRepos}`)
+
+    const query = queryPart.join('+')
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${query}&per_page=${perPage}&page=${page}`, config)
+    return res.data.items
 }
 
 export {fetchUserData}
