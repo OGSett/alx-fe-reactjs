9b7b584ccff451460ee0ea8c80dafb1de599c3b1
ogsett <ecomseth@gmail.com>
2025-08-16 07:24:06 +0100

    editing task01 agn

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,14 +13,14 @@ const Home = () => {
 
 
     return ( 
-        <div className=" max-w-[400px] sm:max-w-[950px] sm:min-w-[456px] md:w-[50%] w-full h-[100dvh] m-auto rounded grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-6 p-2 overflow-scroll md:overflow-hidden   ">
+        <div className=" max-w-[400px] sm:max-w-[950px] sm:min-w-[456px] md:w-[50%] w-full h-[100dvh] m-auto rounded grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[5px] md:gap-6 p-2 overflow-scroll md:overflow-hidden   ">
         {dataArray.map((dataR, index) => (
-        <section key={index} className="lg:min-w-[220px] md:w-[100%] h-[100%] md:h-[60%] bg-gray-300 m-auto rounded-md md:hover:scale-105  hover:shadow-lg hover:shadow-gray-400 transition duration-200 p-2 flex flex-col justify-around items-center cursor-pointer">
-            <span className="font-playfair text-3xl font-semibold text-center md:min-h-[72px] my-2 md:my-0 md:text-[30px] text-[20px]">{dataR.title}</span>
+        <section key={index} className="lg:mix-w-[456px] md:w-[100%] h-[100%] md:h-[80%] lg:h-[60%] bg-gray-300 m-auto rounded-md md:hover:scale-105  hover:shadow-lg hover:shadow-gray-400 transition duration-200 p-2 flex flex-col justify-around items-center cursor-pointer">
+            <span className="font-playfair lg:text-3xl text-2xl font-semibold text-center md:min-h-[72px] my-2 md:my-0 lg:text-[30px] md:text-[20px] text-[15px]">{dataR.title}</span>
             <div className="mx-auto rounded-[50%]">
-                <img src={dataR.image} className="md:w-[200px] md:h-[200px] w-[150px] h-[150px] object-cover rounded-full mx-auto" alt="" />
+                <img src={dataR.image} className="lg:w-[200px] lg:h-[200px] w-[150px] h-[150px] object-cover rounded-full mx-auto" alt="" />
             </div>
-            <p className="font-poppins text-gray-700 min-w-[90%] p-2 md:text-[20px] text-[14px] text-center md:min-h-[170px]">{dataR.summary}</p>
+            <p className="font-poppins text-gray-700 p-2 lg:text-[20px] text-[14px] text-center lg:min-h-[170px]">{dataR.summary}</p>
         </section>
         ))}
         </div>
