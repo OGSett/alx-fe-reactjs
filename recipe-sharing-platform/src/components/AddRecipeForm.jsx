bde10b417b70f764e75fa6f09c1af31f8adabbe4
ogsett <ecomseth@gmail.com>
2025-08-17 00:21:19 +0100

    appaarently it wasnt the last

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -5,27 +5,26 @@ const AddRecipeForm  = () => {
     const [formData, setFormData] = useState({
     title: "",
     ingredients: "",
-    preparation: "",
-    });
+    steps: "",
+    })
 
     const [errors, setErrors] = useState({
         title: false,
         ingredients: false,
-        preparation: false,
+        steps: false,
     });
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prev) => ({ ...prev, [name]: value }));
-        setErrors((prev) => ({ ...prev, [name]: false }));
+        setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+        setErrors((prev) => ({ ...prev, [e.target.name]: false }))
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        let newErrors = {};
+        let newErrors = {}
         Object.keys(formData).forEach((key) => {
-        newErrors[key] = formData[key].trim() === "";
+        newErrors[key] = formData[key].trim() === ""
         });
 
         setErrors(newErrors);
@@ -33,7 +32,7 @@ const AddRecipeForm  = () => {
         if (Object.values(newErrors).some((err) => err)) {
         console.log("Validation failed ");
         } else {
-        console.log("Form submitted ", formData);
+        console.log("Form submitted ", formData)
         }
     };
 
@@ -46,12 +45,12 @@ const AddRecipeForm  = () => {
                     <input type="text" name="title"value={formData.title} className={`p-2 rounded-lg text-center w-full ${  errors.title ? "border-2 border-red-500" : "border border-gray 300" }`} onChange={handleChange} placeholder="Type here!" />
                 </div>
                 <div className="flex flex-col my-2">
-                    <span className="font-poppins text-[20px] my-2 tracking-[0.7px] ">ingredients :</span>
-                    <textarea type="text" name="ingredients"value={formData.ingredients} className={`p-2 rounded-lg text-center  ${  errors.title ? "border-2 border-red-500" : "border border-gray 300" }`} onChange={handleChange} rows='5' cols='30' placeholder="Type here!" />
+                    <span className="font-poppins text-[20px] my-2 tracking-[0.7px] ">Ingredients :</span>
+                    <textarea type="text" name="ingredients"value={formData.ingredients} className={`p-2 rounded-lg text-center  ${  errors.ingredients ? "border-2 border-red-500" : "border border-gray 300" }`} onChange={handleChange} rows='5' cols='30' placeholder="Type here!" />
                 </div>
                 <div className="flex flex-col my-2">
-                    <span className="font-poppins text-[20px] my-2 tracking-[0.7px] ">preparation :</span>
-                    <textarea type="text" name="preparation"value={formData.preparation} className={`p-2 rounded-lg text-center  ${  errors.title ? "border-2 border-red-500" : "border border-gray 300" }`} onChange={handleChange}  rows='5' cols='30' placeholder="Type here!" />
+                    <span className="font-poppins text-[20px] my-2 tracking-[0.7px] ">Preparation steps :</span>
+                    <textarea type="text" name="steps"value={formData.steps} className={`p-2 rounded-lg text-center  ${  errors.steps ? "border-2 border-red-500" : "border border-gray 300" }`} onChange={handleChange}  rows='5' cols='30' placeholder="Type here!" />
                 </div>
                 <button type="submit" className="p-4 text-white mt-4 rounded-md bg-recipe-dark mb-2">Add recipe</button>
             </form>
